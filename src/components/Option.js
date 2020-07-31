1bae4b70de0bdc402bd8290673737836f27f3caf
Archit Gupta <rarchitgupta@gmail.com>
2020-07-31 12:17:51 +0530

    Initial Commit

---
+++ src/components/Option.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Option = (props) => {
+  return (
+    <div className="option">
+      <p className="option__text">
+        {props.count}. {props.optionText}
+      </p>
+
+      <button
+        onClick={(e) => {
+          props.handleDeleteOption(props.optionText);
+        }}
+        className="button button--link"
+      >
+        Remove
+      </button>
+    </div>
+  );
+};
+
+export default Option;
